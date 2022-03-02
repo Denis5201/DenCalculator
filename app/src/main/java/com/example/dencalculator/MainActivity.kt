f2145e5389d1b335e732a42944e662140db38afe
Denis5201 <builukden@yandex.ru>
2022-03-02 20:30:40 +0500

    update 2

--- app/src/main/java/com/example/dencalculator/MainActivity.kt
+++ app/src/main/java/com/example/dencalculator/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.dencalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
+import android.widget.Button
 import android.widget.Toast
 import com.example.dencalculator.databinding.ActivityMainBinding
 
@@ -18,6 +18,25 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        for (num in binding.groupNumber.referencedIds) {
+            val but = findViewById<Button>(num)
+            but.setOnClickListener { binding.textdisplay.append(but.text) }
+        }
+
+        for (s in binding.groupSign.referencedIds) {
+            val but = findViewById<Button>(s)
+            but.setOnClickListener {
+                number1 = binding.textdisplay.text.toString()
+                binding.textdisplay.setText("")
+
+                sign = but.text[0]
+            }
+        }
+
+        binding.buttonDot.setOnClickListener {
+            binding.textdisplay.append(".")
+        }
+
         binding.buttonAC.setOnClickListener {
             binding.textdisplay.setText("")
             number1 = ""
@@ -28,10 +47,10 @@ class MainActivity : AppCompatActivity() {
         binding.changeSign.setOnClickListener {
             val temp = binding.textdisplay.text.toString()
             if (temp != "" && temp != "0")
-            if (temp.toDouble()>0)
-                binding.textdisplay.setText("-$temp")
-            else
-                binding.textdisplay.setText(temp.drop(1))
+                if (temp.toDouble() > 0)
+                    binding.textdisplay.setText("-$temp")
+                else
+                    binding.textdisplay.setText(temp.drop(1))
         }
 
         binding.percent.setOnClickListener {
@@ -39,17 +58,16 @@ class MainActivity : AppCompatActivity() {
 
             if (number1 != "" && number2 != "") {
                 val result = when (sign) {
-                    '+' -> (number1.toDouble() + number2.toDouble()*number1.toDouble()/100).toString()
-                    '-' -> (number1.toDouble() - number2.toDouble()*number1.toDouble()/100).toString()
-                    '*' -> (number1.toDouble() * number2.toDouble()/100).toString()
-                    '/' -> (number1.toDouble() / (number2.toDouble()/100)).toString()
+                    '+' -> (number1.toDouble() + number2.toDouble() * number1.toDouble() / 100).toString()
+                    '-' -> (number1.toDouble() - number2.toDouble() * number1.toDouble() / 100).toString()
+                    '*' -> (number1.toDouble() * number2.toDouble() / 100).toString()
+                    '/' -> (number1.toDouble() / (number2.toDouble() / 100)).toString()
                     else -> ""
                 }
                 binding.textdisplay.setText(result)
             }
 
         }
-
 
         binding.equal.setOnClickListener {
             number2 = binding.textdisplay.text.toString()
@@ -57,49 +75,17 @@ class MainActivity : AppCompatActivity() {
             if (number2 == "0" && sign == '/') {
                 Toast.makeText(this, "Деление на 0", Toast.LENGTH_SHORT).show()
                 binding.textdisplay.setText("")
-            }
-            else if (number1 != "" && number2 != "") {
+            } else if (number1 != "" && number2 != "") {
                 val result = when (sign) {
                     '+' -> (number1.toDouble() + number2.toDouble()).toString()
                     '-' -> (number1.toDouble() - number2.toDouble()).toString()
-                    '*' -> (number1.toDouble() * number2.toDouble()).toString()
-                    '/' -> (number1.toDouble() / number2.toDouble()).toString()
+                    'x' -> (number1.toDouble() * number2.toDouble()).toString()
+                    '÷' -> (number1.toDouble() / number2.toDouble()).toString()
                     else -> ""
                 }
                 binding.textdisplay.setText(result)
             }
         }
     }
-
-    fun onNumberClick(view: View) {
-        val ID = view.id
-        when (ID) {
-            binding.button0.id -> binding.textdisplay.append("0")
-            binding.button1.id -> binding.textdisplay.append("1")
-            binding.button2.id -> binding.textdisplay.append("2")
-            binding.button3.id -> binding.textdisplay.append("3")
-            binding.button4.id -> binding.textdisplay.append("4")
-            binding.button5.id -> binding.textdisplay.append("5")
-            binding.button6.id -> binding.textdisplay.append("6")
-            binding.button7.id -> binding.textdisplay.append("7")
-            binding.button8.id -> binding.textdisplay.append("8")
-            binding.button9.id -> binding.textdisplay.append("9")
-            binding.buttonDot.id -> binding.textdisplay.append(".")
-        }
-    }
-
-    fun onSignClick(view: View) {
-        number1 = binding.textdisplay.text.toString()
-        binding.textdisplay.setText("")
-
-        var ID = view.id
-        when (ID) {
-            binding.sum.id -> sign = '+'
-            binding.sub.id -> sign = '-'
-            binding.multi.id -> sign = '*'
-            binding.division.id -> sign = '/'
-        }
-    }
-
-
+    
 }
